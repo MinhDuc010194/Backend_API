1483e8484363cbafd3c3dc6349dd89fb068a6645
Duc_nm <duc_nm@anybim.vn>
2020-03-19 10:18:22 +0700

    up

---
+++ test/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using test.Entities.DataTransferObjects;
+using test.Entities.Models;
+
+namespace test
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Owner, OwnerDto>();
+
+            CreateMap<Account, AccountDto>();
+
+            CreateMap<OwnerForUpdateDto, Owner>();
+        }
+    }
+}
